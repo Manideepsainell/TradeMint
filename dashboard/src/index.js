c8f5a3268b3ed455d2fbc49dae4774b22dbdddfa
Manideepsai <nmanideepsai25@gmail.com>
2026-01-23 18:37:39 +0530

    chore: update dependencies and app entry configuration

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import ScrollToTop from "./components/ScrollToTop";
 import "./index.css";
-import Home from "./components/Home";
+
+// ✅ If you have AuthProvider in dashboard
+import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/*" element={<Home />} />
-      </Routes>
+      <ScrollToTop />
+      <AuthProvider>
+        <App />
+      </AuthProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
