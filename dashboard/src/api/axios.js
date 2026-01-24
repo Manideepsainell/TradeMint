85cbd571f4c4e7ecb77c3fe0b12b72b195a11bdf
Manideepsai <nmanideepsai25@gmail.com>
2026-01-24 23:01:54 +0530

    Error Resolving

--- dashboard/src/api/axios.js
+++ dashboard/src/api/axios.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+console.log("🔥 BACKEND URL:", process.env.REACT_APP_API_URL);
+
 const api = axios.create({
   baseURL: process.env.REACT_APP_API_URL,
   withCredentials: true,
