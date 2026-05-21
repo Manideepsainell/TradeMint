e31a563c21df3e0b5b7935f94d1febee6f0feb97
Manideepsai <nmanideepsai25@gmail.com>
2026-05-21 12:58:35 +0530

    feat: add funds validation and margin tracking for buy/sell orders

--- backend/controllers/fundsController.js
+++ backend/controllers/fundsController.js
@@ -1,7 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import Funds from "../model/FundsModel.js";
 export const getFunds = asyncHandler(async (req, res) => {
- const funds = await Funds.findOne({ userId: req.user.id });
+ let funds = await Funds.findOne({ userId: req.user.id });
 
 
   if (!funds) {
